11567cf1703de4550fec383bbfa2e1c9a34a3320
Begotten-Traveler <solomondev002@gmail.com>
2025-04-02 14:09:36 +0100

    add: expand function 2 added

---
+++ expand2.js
@@ -0,0 +1,7 @@
+let btnEl2 = document.getElementById("btn-el2");
+let messageEl2 = document.getElementById("message-el2");
+
+function expand2(){
+    let message = "this is my thoughts";
+    messageEl2.textContent = message;
+}
